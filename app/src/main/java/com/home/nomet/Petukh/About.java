4bdaa1361e966ebb76c46fcad31c82176f995297
Владислав <vlad-s-d@ya.ru>
2017-04-21 17:28:37 +0300

    Добавил картинку в главном активити. Сделвл переход в окно "О программе". Все работает, но как то вяло. Попробую запустить на планшете

--- app/src/main/java/com/home/nomet/Petukh/About.java
+++ app/src/main/java/com/home/nomet/Petukh/About.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class About extends AppCompatActivity {
 
@@ -13,8 +14,8 @@ public class About extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
+            setContentView(R.layout.activity_about);
+            Toast.makeText(this, "kek", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/home/nomet/Petukh/MainActivity.java
+++ app/src/main/java/com/home/nomet/Petukh/MainActivity.java
@@ -1,7 +1,12 @@
 package com.home.nomet.Petukh;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent activity_about = new Intent(this, About.class);
+        final PendingIntent p_activity_about = PendingIntent.getActivity(this, 0, activity_about, 0);
+        Button about = (Button)findViewById(R.id.about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    p_activity_about.send();
+                } catch (PendingIntent.CanceledException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
+
+
 }
