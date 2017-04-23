e4b1e88a0fb89037cc61466cb937b2c513587bba
Владислав <vlad-s-d@ya.ru>
2017-04-23 19:11:08 +0300

    поменял api на 19. Формат сделал 24. Будильник работает. Но говорилка не стартует в onCreate. Только по кнопкке. Не работает и в новом потоке.

--- app/src/main/java/com/home/nomet/Petukh/Alarm.java
+++ app/src/main/java/com/home/nomet/Petukh/Alarm.java
@@ -2,12 +2,55 @@ package com.home.nomet.Petukh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
 
 public class Alarm extends AppCompatActivity {
+
+    private static String tts = "Речь Речь Речь";
+    TextToSpeech TTS;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        TTS = new TextToSpeech(this, new OnInitListener() {         //фигня для говорилки
+            @Override
+            public void onInit(int status) {
+
+            }
+        });
+
+
+        /*Thread speak = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                speak();                        //попытка запустить speak в потоке
+            }
+        });
+        speak.start();*/
+
+       /* Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {              //для дебага
+            @Override
+            public void onClick(View v) {
+                tts = "Оно работает";
+                speak();
+            }
+        });*/
+
+
+
+
     }
+
+    private void speak() {
+        TTS.speak(tts,TextToSpeech.QUEUE_ADD,null);             //метод говорения переменной tts string
+    }
+
+
 }
--- app/src/main/java/com/home/nomet/Petukh/AlarmSet.java
+++ app/src/main/java/com/home/nomet/Petukh/AlarmSet.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextClock;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -26,9 +27,10 @@ public class AlarmSet extends AppCompatActivity {
         setContentView(R.layout.activity_alart_set);
 
         final TimePicker Timepick = (TimePicker)findViewById(R.id.TimePicker);
-        Button alarmset = (Button)findViewById(R.id.timeset);                       //работа с интерфейсом и пр
+        Button alarmset = (Button)findViewById(R.id.Set);                       //работа с интерфейсом и пр
         Timepick.setIs24HourView(true);
         final AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        TextClock textclock = (TextClock)findViewById(R.id.textClock);
 
         Intent RING = new Intent(this, Alarm.class );
         final PendingIntent p_RING = PendingIntent.getActivity(this, 0, RING, 0);   //интенты  звонка будильника
@@ -44,15 +46,17 @@ public class AlarmSet extends AppCompatActivity {
         cal.set(Calendar.DATE, cur_cal.get(Calendar.DATE));
         cal.set(Calendar.MONTH, cur_cal.get(Calendar.MONTH));
 
+
+
         alarmset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AlarmSet.this, "Setted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(AlarmSet.this, "Setted", Toast.LENGTH_SHORT).show();                 //установка времени будильника и самого будильника
                 int h = Timepick.getCurrentHour();
                 int m = Timepick.getCurrentMinute();
                 cal.set(Calendar.HOUR_OF_DAY, h);
                 cal.set(Calendar.MINUTE, m);
-                //alarm.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), p_RING);
+                alarm.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), p_RING);
             }
         });
 
