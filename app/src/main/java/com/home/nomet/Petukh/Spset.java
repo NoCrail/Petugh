70fbc1d720afcd4b72ce6ff1fa0e7e4b6d63d5b7
Владислав <vlad-s-d@ya.ru>
2017-05-21 22:49:31 +0400

    Фигачу сохранения для чеков

--- app/src/main/java/com/home/nomet/Petukh/Spset.java
+++ app/src/main/java/com/home/nomet/Petukh/Spset.java
@@ -1,6 +1,7 @@
 package com.home.nomet.Petukh;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,14 +9,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class Spset extends AppCompatActivity {
+    public static final Boolean CheckGMB = false;
+    public static final Boolean CheckWB = false;
+    public static final Boolean CheckTDB = false;
 
+    private SharedPreferences CheckGMSP;
+    private SharedPreferences CheckWSP;
+    private SharedPreferences CheckTDSP;
+
+    private Boolean CheckGMA;
+    private Boolean CheckWA;
+    private Boolean CheckTDA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spset);
+
         Button todoEdit = (Button)findViewById(R.id.TODOEdit);
+        Button save = (Button)findViewById(R.id.save);
+        CheckBox CheckGM = (CheckBox)findViewById(R.id.checkGoodMorning);
+        CheckBox CheckW = (CheckBox)findViewById(R.id.checkWeather);
+        CheckBox CheckTD = (CheckBox)findViewById(R.id.checkRemind);
+
+
         todoEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,7 +43,12 @@ public class Spset extends AppCompatActivity {
                 startActivity(todoEditor);
             }
         });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
     }
 
