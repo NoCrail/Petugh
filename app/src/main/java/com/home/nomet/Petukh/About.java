4ed88921e8a9353e338eafc8c2abb4365538fc12
Владислав <vlad-s-d@ya.ru>
2017-05-23 18:29:14 +0300

    Lдобавил ссылку на гитхаб

--- app/src/main/java/com/home/nomet/Petukh/About.java
+++ app/src/main/java/com/home/nomet/Petukh/About.java
@@ -1,7 +1,11 @@
 package com.home.nomet.Petukh;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
@@ -9,7 +13,14 @@ public class About extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_about);
-
+            Button git = (Button)findViewById(R.id.git);
+            git.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/NoCrail/Petugh"));
+                    startActivity(browserIntent);
+                }
+            });
         }
 
     }
