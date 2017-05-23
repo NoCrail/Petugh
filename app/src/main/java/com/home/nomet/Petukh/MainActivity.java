d48436d24844120de51591fa38184ce0b9a43bfe
Владислав <vlad-s-d@ya.ru>
2017-05-23 18:12:56 +0300

    Почти готово

--- app/src/main/java/com/home/nomet/Petukh/MainActivity.java
+++ app/src/main/java/com/home/nomet/Petukh/MainActivity.java
@@ -2,10 +2,14 @@ package com.home.nomet.Petukh;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 import com.google.gson.GsonBuilder;
@@ -26,18 +30,58 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-
-
-
+        ImageView image = (ImageView)findViewById(R.id.imageView2);
         Button about = (Button)findViewById(R.id.about);
         Button spset = (Button)findViewById(R.id.Speak_set);
         Button set = (Button)findViewById(R.id.timeset);            //кнопки назначаются
+
+        image.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.petukh1, 300, 300));
 
         spset.setOnClickListener(new View.OnClickListener() {
             @Override
